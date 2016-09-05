078c7247a2df3b3641e807548b424566e607016c
Catherine Holloway <milankie@gmail.com>
2016-09-05 16:15:57 -0400

    started making map

--- doc/js/main.js
+++ doc/js/main.js
@@ -33,38 +33,86 @@ var SVG_drawing = function (num_across, num_down, image_height, image_width,
     this.svg_init = false;
     // create a map of the nodes
     this.nodes = [];
-    for(i=0; i<this.num_down;i++)
+    this.new_width = this.image_width/this.num_across;
+    for(i=0; i<this.num_across;i++)
     {
-        var node = {x_pos:50, y_pos:i*50, size:3};
-        this.nodes.push(node);
+        for(j=0; j<this.num_down;j++)
+        {
+            // add the entry node
+            var x_pos = LED_scale*(this.new_width*(this.num_across-i-1)/LED_scale+this.right_margin);
+            var y_pos = LED_scale*((this.image_height/this.num_down)*j/LED_scale+this.bottom_margin);
+            if(j==0)
+            {
+                var next = i+1;
+                var node = {x_pos:x_pos, y_pos:y_pos, size:junction_radius,
+                            name: "pin_"+j,
+                            neighbors: ["entry_"+next+"_"+0, "exit_"+i+"_"+1]};
+                this.nodes.push(node);
+
+            }
+            var node = {x_pos:x_pos + LED_entries[0][0]*LED_scale,
+                        y_pos:y_pos + LED_entries[0][1]*LED_scale,
+                        size:junction_radius, name:"entry_"+i+"_"+j};
+            this.nodes.push(node);
+            var node = {x_pos:x_pos + LED_entries[1][0]*LED_scale,
+                        y_pos:y_pos + LED_entries[1][1]*LED_scale,
+                        size:junction_radius, name:"exit_"+i+"_"+j};
+            this.nodes.push(node);
+        }
     }
+
 };
+var LED_entries = [[4, 50], [103, 50]];
+var LED_dimensions = [106.0, 71.0];
+var LED_scale = .5;
+var junction_radius = 1.0;
+
 
 SVG_drawing.prototype.drawSVG = function(){
-    var LED_dimensions = [106.0, 71.0];
+    function connect_points(i, j)
+    {
+        console.log(drawing.nodes[i].name, drawing.nodes[j].name)
+        drawing.s.line(drawing.nodes[i].x_pos, drawing.nodes[i].y_pos,
+                    drawing.nodes[j].x_pos, drawing.nodes[j].y_pos).attr({ stroke: 'black', 'strokeWidth': 2});
+    }
+    function find_index(name)
+    {
+        for(i=0; i<drawing.nodes.length; i++)
+        {
+            if(drawing.nodes[i].name == name)
+            {
+                return i;
+            }
+        }
+    }
     var LED_points = [[35, 68], [35, 31], [66, 50]];
-    var LED_entries = [[4, 50], [103, 50]];
+
     var aspect_ratio = LED_dimensions[1]/LED_dimensions[0];
-    var new_width = this.image_width/this.num_across;
-    var new_height = new_width*aspect_ratio;
-    var LED_scale = 0.75;
-    var LED_offsets = [new_width*LED_scale/2, new_height*LED_scale];
-    var junction_radius = 0.8;
+    var new_height = this.new_width*aspect_ratio;
+    var LED_offsets = [this.new_width*LED_scale/2, new_height*LED_scale];
     var current_led = [2, 1];
 
     for(i = 0; i < this.num_across; i++)
     {
-        var x_pos = new_width*(this.num_across-i-1)/LED_scale;
+        var x_pos = this.right_margin+this.new_width*(this.num_across-i-1)/LED_scale;
 
         for(j = 0; j< this.num_down;j++)
         {
-            var y_pos = (this.image_height/this.num_down)*j/LED_scale;
+            var y_pos = this.bottom_margin+(this.image_height/this.num_down)*j/LED_scale;
+
             var position = [x_pos+LED_offsets[0], y_pos+LED_offsets[1]];
             var led_clone = this.led_data.clone();
             var myMatrix = new Snap.Matrix();
             myMatrix.scale(LED_scale);
             myMatrix.translate(x_pos, y_pos);
             led_clone.transform(myMatrix);
+            if(j==0)
+            {
+                text = this.s.text(0, 0, i+1);
+                text.transform(myMatrix);
+
+            }
+
             if(i==current_led[0] && j==current_led[1])
             {
                 led_clone.select("path[stroke='#000']").attr({fill: "#ffff00"});
@@ -72,15 +120,22 @@ SVG_drawing.prototype.drawSVG = function(){
             this.s.append(led_clone);
         }
     }
-
+    var pin_0_index = find_index('pin_0');
     for(i = 0; i < this.nodes.length; i++)
     {
         this.s.circle(this.nodes[i].x_pos, this.nodes[i].y_pos,
-                      this.nodes[i].size).attr({ stroke: '#123456', 'strokeWidth': 20, fill: 'blue' });
+                      this.nodes[i].size).attr({ stroke: 'None', 'strokeWidth': 0, fill: 'black' });
+    }
+    // walk through the node tree
+    for(neighbor_index=0; neighbor_index < this.nodes[pin_0_index].neighbors.length; neighbor_index++)
+    {
+        console.log(neighbor_index, this.nodes[pin_0_index].neighbors[neighbor_index]);
+        var neighbor_index = find_index(this.nodes[pin_0_index].neighbors[neighbor_index]);
+        //connect_points(pin_0_index, neighbor_index);
     }
 
-
 };
+
 
 SVG_drawing.prototype.onSVGLoaded = function(data){
     drawing.led_data = data.select("g");
